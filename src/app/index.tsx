aa24342086419fcfc0c388348ae6ab987694bd54
3S Cavalcante <aderc19@gmail.com>
2024-12-10 10:28:13 -0300

    :sparkles: Criação dos componentes da tela de Welcome + Organização

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
-import { Welcome } from "@/components/welcome";
 import { View, Text } from "react-native";
+import { Steps } from "@/components/steps";
+import { Welcome } from "@/components/welcome";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
+      <Steps />
     </View>
   );
 }
--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,6 +1,6 @@
 import { ActivityIndicator } from "react-native";
 import { s } from "./styles";
-import { colors } from "@/styles/colors";
+import { colors } from "@/styles/theme";
 
 export function Loading() {
   return <ActivityIndicator color={colors.green.base} style={s.container} />;

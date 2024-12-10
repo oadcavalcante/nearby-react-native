2b808408ed3aa050a8ed121f12ae111dd09e2e75
3S Cavalcante <aderc19@gmail.com>
2024-12-10 09:38:50 -0300

    :tada: first commit.

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Welcome } from "@/components/welcome";
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+    </View>
+  );
+}

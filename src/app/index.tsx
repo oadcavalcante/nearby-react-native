d35e08e0dc7622fe259fce3acfee808b6d212f67
3S Cavalcante <aderc19@gmail.com>
2024-12-11 12:42:47 -0300

    :sparkles: feat: Adicionando o botão na tela de Welcome.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,17 @@
 import { View, Text } from "react-native";
 import { Steps } from "@/components/steps";
 import { Welcome } from "@/components/welcome";
+import { Button } from "@/components/button";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
+
+      <Button>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
